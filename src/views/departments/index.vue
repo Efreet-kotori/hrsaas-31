5aeec78e06f0cca576aa3397c021f94e0bc022b9
kotoriefreet <1063377232@qq.com>
2022-08-05 11:32:06 +0800

    树形

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <TreeTools :treeNode="company" :isRoot="true" />
+        <br />
+        <!-- 树形 -->
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <TreeTools :treeNode="data" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './compoents/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' },
+      ],
+      defaultProps: {
+        label: 'name',
+        // children: 'child',
+      },
+      company: { name: '传智教育', manager: '负责人' },
+    }
   },
-
+  components: {
+    TreeTools,
+  },
   created() {},
 
-  methods: {}
+  methods: {},
 }
 </script>
 
