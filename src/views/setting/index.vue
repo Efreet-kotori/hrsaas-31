142eabc27c980cbe5adc8cfa70a4cbbac5ae5fdd
kotoriefreet <1063377232@qq.com>
2022-08-11 11:47:05 +0800

    完成角色的部分处理

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,20 +1,167 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height: 60px">
+              <el-button
+                icon="el-icon-plus"
+                size="small"
+                type="primary"
+                @click="addDialogVisible = true"
+                >新增角色</el-button
+              >
+            </el-row>
+            <!-- 表格 -->
+            <el-table border :data="tableData">
+              <el-table-column type="index" label="序号" width="60" />
+              <el-table-column prop="name" label="角色名称" width="240" />
+              <el-table-column prop="description" label="描述" />
+              <el-table-column label="操作" width="240">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+            <!-- 分页组件 -->
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <!-- 分页组件 -->
+              <el-pagination
+                layout="prev,pager,next,sizes"
+                :total="total"
+                :page-size="pagesize"
+                :page-sizes="[3, 5, 10, 20]"
+                @current-change="currentChange"
+                @size-change="handleSizeChange"
+              />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              type="info"
+              show-icon
+              :closable="false"
+            />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
+
+    <!-- 添加角色对话框 -->
+    <el-dialog
+      @close="dialogClose"
+      title="新增角色"
+      :visible.sync="addDialogVisible"
+      width="50%"
+    >
+      <el-form
+        :model="addRoleForm"
+        :rules="addRoleFormRules"
+        ref="form"
+        label-width="80px"
+      >
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="addRoleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="description">
+          <el-input v-model="addRoleForm.description"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="onClose">取 消</el-button>
+        <el-button @click="onAddRole" type="primary">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getRolesApi, addRolesApi } from '@/api/role'
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'first',
+      tableData: [],
+      total: 0,
+      page: 1,
+      pagesize: 3,
+      addDialogVisible: false,
+      addRoleForm: {
+        name: '',
+        description: '',
+      },
+      addRoleFormRules: {
+        name: [{ required: true, message: '请输入角色名称', trigger: 'blur' }],
+      },
+    }
   },
 
-  created() {},
+  created() {
+    this.getRoles()
+  },
 
-  methods: {}
+  methods: {
+    async getRoles() {
+      const { rows, total } = await getRolesApi({
+        page: this.page,
+        pagesize: this.pagesize,
+      })
+      console.log(rows)
+      this.tableData = rows
+      this.total = total
+    },
+    currentChange(val) {
+      this.page = val
+      this.getRoles()
+    },
+    handleSizeChange(val) {
+      this.pagesize = val
+      this.getRoles()
+    },
+    onClose() {
+      this.addDialogVisible = false
+    },
+    async onAddRole() {
+      await this.$refs.form.validate()
+      await addRolesApi(this.addRoleForm)
+      this.$message.success('添加成功')
+      this.addDialogVisible = false
+      this.getRoles()
+    },
+    dialogClose() {
+      this.$refs.form.resetFields()
+      this.addRoleForm.description = ''
+    },
+  },
 }
 </script>
 
